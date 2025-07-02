23fbe95a7db7e8b55af994fa91a6aca6213bcaef
Francis-Ngigi <nickoze61@gmail.com>
2025-07-02 09:11:17 +0300

    Update train.py

--- train.py
+++ train.py
@@ -68,16 +68,12 @@ class ChatDataset(Dataset):
         self.n_samples = len(value_train)
         self.x_data = value_train
         self.y_data = othervalue_train
-
-    # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
     # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
-
-
 dataset = ChatDataset()
 our_our_train_loader = DataLoader(
     dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0
@@ -86,20 +82,14 @@ our_our_train_loader = DataLoader(
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = NeuralNet(our_input_size, hidden_size, our_output_size).to(device)
-
-# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-# Train the model
 for epoch in range(our_epochs):
     for words, labels in our_our_train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-
-        # Forward pass
         outputs = model(words)
-        # if y would be one-hot, we must apply
+        
         # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
 
@@ -110,8 +100,6 @@ for epoch in range(our_epochs):
 
     if (epoch + 1) % 100 == 0:
         print(f"Epoch [{epoch+1}/{our_epochs}], Loss: {loss.item():.4f}")
-
-
 print(f"Last loss: {loss.item():.4f}")
 
 data = {
@@ -125,5 +113,4 @@ data = {
 
 FILE = "data.pth"
 torch.save(data, FILE)
-# we complete our training
 print(f"training complete. file saved to {FILE}")
